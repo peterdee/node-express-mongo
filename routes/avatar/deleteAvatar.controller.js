ecae5894d8a27b8d3adc706b9de26a813d60746c
peterorbb <orbitalist@gmail.com>
2020-02-16 15:10:11 +0300

    Avatar APIs: delete avatar

--- routes/avatar/deleteAvatar.controller.js
+++ routes/avatar/deleteAvatar.controller.js
@@ -8,6 +8,35 @@ const { RESPONSE_STATUSES: rs, SERVER_MESSAGES: sm } = require('../../config');
  * @param req {object} - request object
  * @param res {object} - response object
  * @returns {Promise<void>}
+ * 
+ * apiDoc:
+ * @api {delete} /api/v1/avatar Delete avatar
+ * @apiSampleRequest http://localhost:2211/api/v1/avatar
+ * @apiName DeleteAvatar
+ * @apiGroup AVATAR
+ * @apiDescription This API allows user to delete his / her avatar
+ *
+ * @apiHeader {String} X-Access-Token Access token
+ *
+ * @apiHeaderExample {json} Request-Example:
+ * {
+ *   "X-Access-Token": "accessToken"
+ * }
+ * 
+ * @apiSuccess (200) {Number} datetime Response timestamp
+ * @apiSuccess (200) {String} info OK
+ * @apiSuccess (200) {String} misc NO_ADDITIONAL_INFORMATION
+ * @apiSuccess (200) {String} request /api/v1/avatar [DELETE]
+ * @apiSuccess (200) {Number} status 200
+ *
+ * @apiSuccessExample {json} OK
+ * {
+ *   "datetime": 1570104879307,
+ *   "info": "OK",
+ *   "misc": "NO_ADDITIONAL_INFORMATION",
+ *   "request": "/api/v1/avatar [DELETE]",
+ *   "status": 200
+ * }
  */
 module.exports = async (req, res) => {
   try {
@@ -16,16 +45,22 @@ module.exports = async (req, res) => {
       return basic(req, res, rs[200], sm.ok);
     }
 
-    // update the User record
-    await db.User.updateOne(
-      {
-        _id: req.id,
-      },
-      {
-        avatarLink: '',
-        updated: getSeconds(),
-      },
-    );
+    // get Image UID from the avatar link
+    const uid = req.user.avatarLink.split('/').reverse()[0];
+
+    // delete the Image record and update the User record
+    await Promise.all([
+      db.Image.deleteOne({ uid }),
+      db.User.updateOne(
+        {
+          _id: req.id,
+        },
+        {
+          avatarLink: '',
+          updated: getSeconds(),
+        },
+      ),
+    ]);
 
     return basic(req, res, rs[200], sm.ok);
   } catch (error) {
