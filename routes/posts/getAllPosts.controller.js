1c5ffceb4888e8905425970acb1073593fdae24f
Peter <p.dyumin@dunice.net>
2020-02-23 19:45:45 +0300

    Posts APIs

--- routes/posts/getAllPosts.controller.js
+++ routes/posts/getAllPosts.controller.js
@@ -1,5 +1,6 @@
 const { data, internalError } = require('../../services/responses');
 const db = require('../../db');
+const { formatPaginatedResponse } = require('../../services/utilities');
 const { RESPONSE_STATUSES: rs, SERVER_MESSAGES: sm } = require('../../config');
 
 /**
@@ -10,8 +11,24 @@ const { RESPONSE_STATUSES: rs, SERVER_MESSAGES: sm } = require('../../config');
  */
 module.exports = async (req, res) => {
   try {
-    const Posts = await db.Post.find({ isDeleted: false });
-    return data(req, res, rs[200], sm.ok, { posts: Posts });
+    const { pagination } = req;
+
+    const [count, posts] = await Promise.all([
+      db.Post.count(),
+      db.Post.find(
+        {
+          isDeleted: false,
+        },
+        null,
+        {
+          limit: pagination.limit,
+          skip: pagination.offset,
+        },
+      ),
+    ]);
+    const formattedData = formatPaginatedResponse(count, posts, pagination);
+
+    return data(req, res, rs[200], sm.ok, formattedData);
   } catch (error) {
     return internalError(req, res, error);
   }
